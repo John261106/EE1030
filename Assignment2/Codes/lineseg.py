a6b0989dec183107449e1e8d2f653e41ca9e9446
John Bobby <johnbobby2006@gmail.com>
2024-08-19 15:33:50 +0530

    2nd Assignment

--- Assignment2/Codes/lineseg.py
+++ Assignment2/Codes/lineseg.py
@@ -1,37 +1,62 @@
+#Code by GVV Sharma
+#September 12, 2023
+#Revised July 21, 2024
+#released under GNU GPL
+#Secion Formula
+
+
+import sys                                          #for path to external scripts
+sys.path.insert(0, '/home/john-bobby/EE1030/matgeo/codes/CoordGeo')        #path to my scripts
 import numpy as np
+import numpy.linalg as LA
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+#local imports
+from line.funcs import *
+from triangle.funcs import *
+from conics.funcs import circ_gen
 
 
-A = (4, -3)
-B = (8, 5)
-P = (7, 3)
+
+#Given points
+A = np.array(([4,-3])).reshape(-1,1)
+B = np.array(([8,5])).reshape(-1,1)
+P = np.array(([7,3])).reshape(-1,1)
 
 
-x_coords = [A[0], B[0], P[0], A[0]]
-y_coords = [A[1], B[1], P[1], A[1]]
 
+#Generating all lines
+x_AB = line_gen(A,B)
 
-plt.figure(figsize=(8, 6))
+#Plotting all lines
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
 
-
-plt.plot(x_coords, y_coords, 'bo-') 
-
-
-plt.text(A[0], A[1], f'A{A}', fontsize=12, verticalalignment='bottom', horizontalalignment='right')
-plt.text(B[0], B[1], f'B{B}', fontsize=12, verticalalignment='bottom', horizontalalignment='right')
-plt.text(P[0], P[1], f'P{P}', fontsize=12, verticalalignment='bottom', horizontalalignment='right')
-
-
-plt.title('Plot of Points A, B, and P')
-plt.xlabel('X coordinate')
-plt.ylabel('Y coordinate')
-
-
-plt.gca().set_aspect('equal', adjustable='box')
-
-
-plt.grid(True)
-
+#Labeling the coordinates
+tri_coords = np.block([[A,B,P]])
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['A','B','P']
+for i, txt in enumerate(vert_labels):
+    #plt.annotate(txt, # this is the text
+    plt.annotate(f'{txt}\n({tri_coords[0,i]:.0f}, {tri_coords[1,i]:.0f})',
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(20,-10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+# use set_position
+ax = plt.gca()
+#ax.spines['top'].set_color('none')
+#ax.spines['left'].set_position('zero')
+#ax.spines['right'].set_color('none')
+#ax.spines['bottom'].set_position('zero')
+ax.spines['left'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+#plt.xlabel('$x$')
+#plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
 
 plt.show()
-
