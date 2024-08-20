50fce0d684752517172f71dde6cda94894ee0ab6
John Bobby <johnbobby2006@gmail.com>
2024-08-20 15:57:33 +0530

    2nd Assignment

--- Assignment2/Codes/lineseg.py
+++ Assignment2/Codes/lineseg.py
@@ -2,11 +2,11 @@
 #September 12, 2023
 #Revised July 21, 2024
 #released under GNU GPL
-#Secion Formula
+#Point Vectors
 
 
 import sys                                          #for path to external scripts
-sys.path.insert(0, '/home/john-bobby/EE1030/matgeo/codes/CoordGeo')        #path to my scripts
+sys.path.insert(0, '/home/john-bobby/MyRepos/matgeo/codes/CoordGeo')        #path to my scripts
 import numpy as np
 import numpy.linalg as LA
 import matplotlib.pyplot as plt
@@ -18,19 +18,25 @@ from triangle.funcs import *
 from conics.funcs import circ_gen
 
 
+#if using termux
+import subprocess
+import shlex
+#end if
 
+I = np.eye((2))
+e1 = I[:,0].reshape(-1, 1)
 #Given points
-A = np.array(([4,-3])).reshape(-1,1)
-B = np.array(([8,5])).reshape(-1,1)
-P = np.array(([7,3])).reshape(-1,1)
-
-
+A = np.array(([4, -3])).reshape(-1,1) 
+B = np.array(([8, 5])).reshape(-1,1)  
+P = np.array(([7, 3])).reshape(-1,1)
 
 #Generating all lines
 x_AB = line_gen(A,B)
+x_BP = line_gen(B,P)
 
 #Plotting all lines
 plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BP[0,:],x_BP[1,:],label='$PB$')
 
 #Labeling the coordinates
 tri_coords = np.block([[A,B,P]])
@@ -41,22 +47,18 @@ for i, txt in enumerate(vert_labels):
     plt.annotate(f'{txt}\n({tri_coords[0,i]:.0f}, {tri_coords[1,i]:.0f})',
                  (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
                  textcoords="offset points", # how to position the text
-                 xytext=(20,-10), # distance from text to points (x,y)
+                 xytext=(20,5), # distance from text to points (x,y)
                  ha='center') # horizontal alignment can be left, right or center
+
 # use set_position
 ax = plt.gca()
-#ax.spines['top'].set_color('none')
-#ax.spines['left'].set_position('zero')
-#ax.spines['right'].set_color('none')
-#ax.spines['bottom'].set_position('zero')
-ax.spines['left'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.spines['bottom'].set_visible(False)
+ax.spines['top'].set_color('none')
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_position('zero')
 #plt.xlabel('$x$')
 #plt.ylabel('$y$')
 plt.legend(loc='best')
 plt.grid() # minor
 plt.axis('equal')
-
-plt.show()
+plt.savefig("Fig1.png")
